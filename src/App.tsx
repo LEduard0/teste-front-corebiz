268ee4d1734a8c2cb7fdead27fb5dac057cbe688
Luiz Eduardo <leduardo152@gmail.com>
2021-07-28 01:12:03 -0300

    add footer component and icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "./components/Header";
 import Shelf from "./components/Shelf";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <Shelf shelfTitle="Mais Vendidos" />
+      <Footer></Footer>
     </div>
   );
 }
