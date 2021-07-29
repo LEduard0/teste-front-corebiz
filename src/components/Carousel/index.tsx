8468022ee173f3467eba275cf5463c1ce9632c7f
Luiz Eduardo <leduardo152@gmail.com>
2021-07-29 00:00:46 -0300

    carousel desktop adjustments

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import Slider from "react-slick";
 
 import bannerMobile from "../../assets/images/bannerMobile.svg";
+import bannerDesktop from "../../assets/images/bannerDesktop.svg";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import "./styles.css";
+
 const settings = {
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+const settingsDesk = {
   arrows: false,
   dots: true,
   infinite: true,
@@ -18,20 +30,38 @@ const settings = {
 const Carousel: React.FC = () => {
   return (
     <>
-      <Slider {...settings}>
-        <img
-          src={bannerMobile}
-          alt="Mobile Banner"
-          width="100%"
-          height="auto"
-        />
-        <img
-          src={bannerMobile}
-          alt="Mobile Banner"
-          width="100%"
-          height="auto"
-        />
-      </Slider>
+      <div className="carouselMobile">
+        <Slider {...settings}>
+          <img
+            src={bannerMobile}
+            alt="Mobile Banner"
+            width="100%"
+            height="auto"
+          />
+          <img
+            src={bannerMobile}
+            alt="Mobile Banner"
+            width="100%"
+            height="auto"
+          />
+        </Slider>
+      </div>
+      <div className="carouselDesktop">
+        <Slider {...settingsDesk}>
+          <img
+            src={bannerDesktop}
+            alt="Desktop Banner"
+            width="100%"
+            height="auto"
+          />
+          <img
+            src={bannerDesktop}
+            alt="Desktop Banner"
+            width="100%"
+            height="auto"
+          />
+        </Slider>
+      </div>
     </>
   );
 };
