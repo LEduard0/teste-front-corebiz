7abe791962c692f8a64cd9f733d49e0ac6a22b04
Luiz Eduardo <leduardo152@gmail.com>
2021-07-28 22:34:02 -0300

    add  shelf functions and validations

--- src/components/Shelf/index.tsx
+++ src/components/Shelf/index.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import StarRatingComponent from "react-star-rating-component";
+
+import apiCorebiz from "../../services/apiCorebiz";
+import { flagIcon } from "../../assets/icons/icons";
 
 import "./styles.css";
 
@@ -8,11 +13,111 @@ import "slick-carousel/slick/slick-theme.css";
 interface IShelfProps {
   shelfTitle?: string;
 }
+interface IproductList {
+  productId: number;
+  productName: string;
+  stars: number;
+  imageUrl: string;
+  price: number;
+  listPrice: number;
+  installments: {
+    quantity: number;
+    value: number;
+  }[];
+}
+
+const slickSettings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+  ],
+};
 
 const Shelf: React.FC<IShelfProps> = ({ shelfTitle }) => {
+  const [product, setProducts] = useState<IproductList[]>();
+
+  const formatPrices = (value: number) => {
+    const price = value.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return price;
+  };
+
+  useEffect(() => {
+    apiCorebiz
+      .get("/products")
+      .then(({ data }) => {
+        setProducts(data);
+      })
+      .catch((error) => {});
+  }, []);
+
   return (
     <div className={"shelfContainer"}>
       <div className={"shelfTitle"}>{shelfTitle}</div>
+      <Slider {...slickSettings}>
+        {product?.map(
+          (
+            { imageUrl, productName, price, listPrice, installments, stars },
+            index
+          ) => (
+            <div key={index} className={"shelfInnerContainer"}>
+              <div className={"shelfWrapper"}>
+                <img src={imageUrl} alt="shelf product" />
+                {price < listPrice ? flagIcon : null}
+                <div className={"shelfInfo"}>
+                  <span className={"shelfProductName"}>{productName}</span>
+                  <StarRatingComponent
+                    name="Stars"
+                    starCount={5}
+                    value={stars}
+                    starColor="#F8475F"
+                    emptyStarColor="#c4c4c4"
+                  />
+                  <span className={"shelfPriceList"}>
+                    {price < listPrice
+                      ? `de ${formatPrices(listPrice / 100)}`
+                      : null}
+                  </span>
+                  <span className={"shelfPrice"}>
+                    por {formatPrices(price / 100)}
+                  </span>
+                  <span className={"shelfInstallments"}>
+                    {installments.length
+                      ? `ou em ${installments[0].quantity}x de ${formatPrices(
+                          installments[0].value / 100
+                        )}`
+                      : null}
+                  </span>
+                  <button className={"shelfAddToCart"} onClick={() => {}}>
+                    Comprar
+                  </button>
+                </div>
+              </div>
+            </div>
+          )
+        )}
+      </Slider>
     </div>
   );
 };
