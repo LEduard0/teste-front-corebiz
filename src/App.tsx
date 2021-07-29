8abaa7d0a513277afd3949eb8a653e34684a3b7b
Luiz Eduardo <leduardo152@gmail.com>
2021-07-28 21:52:53 -0300

    mobile adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Carousel from "./components/Carousel";
 import Shelf from "./components/Shelf";
 import Newsletter from "./components/Newsletter";
 import Footer from "./components/Footer";
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Carousel />
       <Shelf shelfTitle="Mais Vendidos" />
       <Newsletter />
       <Footer />
